22d2b5768ffd49bf47f022857dcef26b13152d17
rigwild <contact@asauvage.fr>
2019-09-25 14:48:03 +0200

    First release

---
+++ README.md
@@ -0,0 +1,16 @@
+# discord-clear-messages
+## Configuration
+Copy [`.env.example`](./.env.example) to `.env` and fill its configuration.
+
+Install dependencies
+```sh
+yarn
+```
+
+Start the bot
+```sh
+yarn start
+```
+
+## License
+[The MIT license](./LICENSE)
--- server.js
+++ server.js
@@ -3,43 +3,30 @@ import { TextChannel, DMChannel, GroupDMChannel } from 'discord.js'
 
 import { SERVER_PORT } from './config'
 import { bot } from './bot'
+import { asyncMiddleware, extractProfile } from './utils'
 
-const app = express()
-
-/**
- * Extract profile data from a Discord user
- * @param {import('discord.js').User} discordUser Targetted user
- * @eturns User profile
- */
-const extractProfile = discordUser => ({
-  id: discordUser.id,
-  username: discordUser.username,
-  discriminator: discordUser.discriminator,
-  avatar: discordUser.avatarURL ? discordUser.avatarURL.replace('size=2048', 'size=32') : null
-})
-
-const routes = express.Router()
+const api = express.Router()
 
 // Get guilds data
-routes.get('/guilds', (req, res) => {
+api.get('/guilds', asyncMiddleware((req, res) => {
   const guilds = bot.guilds
     .map(x => ({ id: x.id, name: x.name, icon: x.iconURL }))
     .sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()))
   res.json({ data: guilds })
-})
+}))
 
 // Get a guild text channels
-routes.get('/guild/:guildId', (req, res) => {
+api.get('/guild/:guildId', asyncMiddleware((req, res) => {
   const { guildId } = req.params
   const guildChannels = bot.guilds.get(guildId).channels
     .filter(x => x instanceof TextChannel)
     .map(x => ({ id: x.id, name: x.name, category: x.parent ? x.parent.name : undefined }))
     .sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()))
   res.json({ data: guildChannels })
-})
+}))
 
 // Get private messages data
-routes.get('/dmChannels', (req, res) => {
+api.get('/dmChannels', asyncMiddleware((req, res) => {
   const dmChannels = bot.channels
     .filter(x => x instanceof DMChannel || x instanceof GroupDMChannel)
     .map(x => {
@@ -59,10 +46,71 @@ routes.get('/dmChannels', (req, res) => {
         }
     })
   res.json({ data: dmChannels })
-})
+}))
 
-app.use('/api', routes)
+api.post('/purge', asyncMiddleware(async (req, res) => {
+  const { channelId, amount: amountRaw = 9999999, deleteDelay: deleteDelayRaw = 200 } = req.body
+  const amount = parseInt(amountRaw, 10)
+  if (isNaN(amount)) throw new Error('The amount of messages to delete is not a valid integer.')
+  const deleteDelay = parseInt(deleteDelayRaw, 10)
+  if (isNaN(deleteDelay)) throw new Error('The delete delay is not a valid integer.')
+
+  const channel = bot.channels.get(channelId)
+  if (!(channel instanceof TextChannel) && !(channel instanceof DMChannel) && !(channel instanceof GroupDMChannel))
+    throw new Error('The selected channel is not a text-based channel.')
+
+  const userId = bot.user.id
+  let lastInspectedMessageSnowflake = '999999999999999999'
+
+  let deletionCount = 0
+  let continuePurge = true
+  while (continuePurge) {
+    const messages = await channel.fetchMessages({ limit: 100, before: lastInspectedMessageSnowflake })
+
+    if (messages.size < 100 && messages.filter(x => x.author.id === userId).size === 0) {
+      continuePurge = false
+      continue
+    }
+
+    // Update last message snowflake for next iteration
+    lastInspectedMessageSnowflake = messages.first().id
+
+    for (const aMessage of messages.values()) {
+      // Check if the max amount was reached
+      if (amount <= deletionCount) {
+        continuePurge = false
+        continue
+      }
+
+      // Update last message snowflake for next iteration
+      lastInspectedMessageSnowflake = aMessage.id
+
+      // Check the message was sent by the user
+      if (aMessage.author.id !== userId || !aMessage.deletable) continue
+      await aMessage.delete()
+      deletionCount++
+
+      // Wait 200ms before deleting next message (dodge self-bot ban)
+      await new Promise(resolve => setTimeout(resolve, deleteDelay))
+    }
+  }
+  res.json({ data: deletionCount })
+}))
+
+// Load the server configuration
+const app = express()
+// Parse JSON input
+app.use(express.json())
+// Load api routes in the server
+app.use('/api', api)
+// Load static front-end
 app.use('/', express.static('public'))
+// Middleware to handle middleware errors
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.status(400).json({ message: err.message })
+  next()
+})
 
 export const startServer = async () => {
   console.log('Starting server...')
